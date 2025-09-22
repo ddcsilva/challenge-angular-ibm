7855f43b2b06601d12493d38a94b31414ccb4c31
Danilo Silva <danilo.silva@msn.com>
2025-09-22 18:27:52 -0300

    feat: Adiciona configuração do ESLint ao projeto Angular, incluindo regras personalizadas para componentes e diretivas, além de arquivos de configuração para linting e dependências atualizadas no package.json e package-lock.json.

--- ibm-workspace/projects/angular-challenge/src/app/app.component.ts
+++ ibm-workspace/projects/angular-challenge/src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'ibm-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [],
+  template: '',
 })
-export class AppComponent {
-  title = 'angular-challenge';
-}
+export class AppComponent {}
